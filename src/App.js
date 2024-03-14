abc4f8f8a8e8b55d2254b252afc8cbed642c3915
Robin Doirin <robindoirin@gmail.com>
2024-03-13 20:45:38 -0400

    added featured section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Nav from "./components/nav";
 import Landing from "./components/landing";
 import Highlights from "./components/Highlights";
+import Featured from "./components/Featured";
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
       <Landing/>
       <Highlights/>
+      <Featured/>
     </div>
   );
 }
