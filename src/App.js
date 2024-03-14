8e31e2dea4bd7a708f8d209cde764fd2589379e7
Robin Doirin <robindoirin@gmail.com>
2024-03-14 11:33:35 -0400

    added discounted, explore, and footer sections

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Nav from "./components/nav";
 import Landing from "./components/landing";
 import Highlights from "./components/Highlights";
 import Featured from "./components/Featured";
+import Discounted from "./components/Discounted";
+import Explore from "./components/Explore";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
@@ -9,6 +12,9 @@ function App() {
       <Landing/>
       <Highlights/>
       <Featured/>
+      <Discounted/>
+      <Explore/>
+      <Footer/>
     </div>
   );
 }
