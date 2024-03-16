244aa5eec99a828da42f2b49f255f2d5634f18c2
Robin Doirin <robindoirin@gmail.com>
2024-03-16 05:46:40 -0400

    added bookinfo routed every link. coded a reconmmended section

---
+++ src/pages/BookInfo.jsx
@@ -0,0 +1,79 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import Rating from "../components/UI/Rating";
+import Price from "../components/UI/Price";
+import Book from "../components/UI/Book";
+
+const BookInfo = ({ books }) => {
+  const { id } = useParams();
+  const book = books.find((book) => +book.id === +id);
+
+  return (
+    <div id="books__body">
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="book" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
+            </div>
+            <div className="book__selected">
+              <figure className="book__slected--figure">
+                <img src={book.url} />
+              </figure>
+              <div className="book__selected--description">
+                <h2 className="book__selected--title">{book.title}</h2>
+                <Rating rating={book.rating} />
+                <div className="book__selected--price">
+                  <Price
+                    originalPrice={book.originalPrice}
+                    salePrice={book.salePrice}
+                  />
+                </div>
+                <div className="book__summary">
+                  <div className="book__summary--title">summary</div>
+                  <h3 className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+                    qui dolorem dolores quam officia consectetur nemo debitis
+                    nesciunt distinctio aliquam quod architecto neque commodi,
+                    culpa dolor. Nulla illum quae tempora.
+                  </h3>
+                  <h3 className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+                    qui dolorem dolores quam officia consectetur nemo debitis
+                    nesciunt distinctio aliquam quod architecto neque commodi,
+                    culpa dolor. Nulla illum quae tempora.
+                  </h3>
+                </div>
+                <button className="btn">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="books__container">
+          <div className="row">
+            <div className="books__selected--top">
+              <h2 className="book__selected--title--top">Recommended Books</h2>
+            </div>
+            <div className="books">
+              {books
+                .filter((book) => book.rating === 5 && +book.id !== +id)
+                .slice(0, 4)
+                .map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default BookInfo;
